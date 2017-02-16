2e03d0c4e3a7a2a5697312bc1d4f618f78f3e8ec
rayGreycloud <rayGreycloud@gmail.com>
2017-02-16 08:44:08 -0800

    Add graphql to express

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true // Built-in dev app
+}));
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
