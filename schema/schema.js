dd36a5799dd3c48bb355dfc07ee2eb20d4347780
rayGreycloud <rayGreycloud@gmail.com>
2017-02-16 09:07:54 -0800

    Add RootQuery to schema and import schema into server

--- schema/schema.js
+++ schema/schema.js
@@ -1,10 +1,20 @@
 const graphql = require('graphql');
+const _ = require('lodash');
+
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLSchema
 } = graphql;
 
+// Hard-coded data list for initial demo
+const users = [
+  { id: '23', firstName: 'Bill', age: 44},
+  { id: '66', firstName: 'Karen', age: 33}
+];
+
+// Define graphql user type
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
@@ -12,4 +22,23 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
   }
+});
+
+// Needed to tell graphql what to grab
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      // Most important-used to return data from datastore
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use('/graphql', expressGraphQL({
+  schema, // ES6 syntax
   graphiql: true // Built-in dev app
+
 }));
 
 app.listen(3000, () => {
